3fac0debe1a3c74ac3a383ec2cd0b55be4639743
Andrew Guinn <Andrew.Guinn@jhuapl.edu>
2022-05-23 14:58:22 -0400

    Added send execute ASP applicable to layered attestation context

---
+++ src/asps/send_execute_tcp_asp.c
@@ -0,0 +1,206 @@
+/*
+ * Copyright 2022 United States Government
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/*! \file
+ * This ASP sends an execute contract to another AM, and sends the resulting
+ * measurement contract to the invoking APB for further processing.
+ *
+ * The ASP does not have the ability to negotiate with an AM, so it should be
+ * connecting to a 'trusted' (skip-negotiation) channel on the peer AM.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <util/util.h>
+#include <util/maat-io.h>
+#include <util/xml_util.h>
+#include <util/signfile.h>
+#include <asp/asp-api.h>
+#include <common/asp-errno.h>
+
+#include <client/maat-client.h>
+
+#define ASP_NAME        "send_execute_tcp_asp"
+
+#define RASP_AM_COMM_TIMEOUT 1000
+
+int asp_init(int argc UNUSED, char *argv[] UNUSED)
+{
+    asp_loginfo("Initialized send_execute_tcp ASP\n");
+    return ASP_APB_SUCCESS;
+}
+
+int asp_exit(int status UNUSED)
+{
+    asp_loginfo("Exiting send_execute_tcp ASP\n");
+    return ASP_APB_SUCCESS;
+}
+
+/**
+ * Finds the appropriate phrase for the resource passed.
+ *
+ * XXX: Currently only implemented for runtime_meas; add more.
+ * XXX: Later should probably implement a form of requestor ASP policy.
+ *
+ * Returns copland phrase for resource if found, NULL otherwise
+ */
+static char *find_phrase(char *resource)
+{
+    char *out = NULL;
+
+    if(strcmp(resource, "runtime_meas") == 0) {
+        out = strdup("(KIM runtime_meas)");
+    } else if (strcmp(resource, "pkginv") == 0) {
+        out = strdup("((USM pkginv) -> SIG)");
+    } else {
+        dlog(0, "Unable to find copland phrase for resource\n");
+        return NULL;
+    }
+
+    return out;
+}
+
+static int send_to_attester_listen_for_result(int attester_chan, char *resource,
+					      char *certfile, char *keyfile,
+					      char *keypass, char *nonce,
+					      char *tpmpass, int sign_tpm,
+					      char **out, size_t *out_size)
+{
+    int ret_val           = 0;
+    int eof_enc           = 0;
+    size_t csize          = 0;
+    size_t bytes_read     = 0;
+    size_t resultsz       = 0;
+    char *result          = NULL;
+    char *phrase          = NULL;
+    xmlChar *exe_contract = NULL;
+
+    phrase = find_phrase(resource);
+    if(!phrase) {
+        dlog(0, "Error: could not find phrase for resource %s\n", resource);
+        ret_val = -1;
+        goto phrase_error;
+    }
+
+    //Create and send execute contract and listen for response
+    ret_val = create_execute_contract(MAAT_CONTRACT_VERSION,
+                                      sign_tpm ? SIGNATURE_TPM : SIGNATURE_OPENSSL,
+                                      phrase, certfile, keyfile, keypass, nonce, tpmpass,
+                                      &exe_contract, &csize);
+    if(ret_val != 0 || exe_contract == NULL) {
+        dlog(0, "create_execute_contract failed: %d\n", ret_val);
+        ret_val = -1;
+        goto contract_error;
+    }
+
+    ret_val = maat_write_sz_buf(attester_chan, exe_contract, csize, NULL, 2);
+    xmlFree(exe_contract);
+    if(ret_val != 0) {
+        dlog(0, "Error sending request. returned status is %d: %s\n", ret_val,
+             strerror(-ret_val));
+        ret_val = -1;
+        goto contract_error;
+    }
+
+    //Receieve response from the attester
+    ret_val = maat_read_sz_buf(attester_chan, &result, &resultsz,
+			       &bytes_read, &eof_enc, RASP_AM_COMM_TIMEOUT, -1);
+    if(ret_val != 0) {
+        dlog(1, "Error reading response. returned status is %d: %s\n", ret_val,
+             strerror(ret_val < 0 ? -ret_val : ret_val));
+        ret_val = -1;
+        goto recv_error;
+    } else if(eof_enc != 0) {
+        dlog(0, "Error: unexpected EOF encountered reading result from attester\n");
+        ret_val = -1;
+        goto recv_error;
+    }
+
+    *out = result;
+    *out_size = resultsz;
+
+    ret_val = 0;
+
+recv_error:
+    free(result);
+contract_error:
+    free(phrase);
+ phrase_error:
+    return ret_val;
+}
+
+int asp_measure(int argc, char *argv[])
+{
+    dlog(4, "In send_execute_tcp ASP\n");
+    int out_fd                      = -1;
+    int targ_fd                     = -1;
+
+    char *resource                  = NULL;
+    char *certfile                  = NULL;
+    char *keyfile                   = NULL;
+    char *keypass                   = NULL;
+    char *nonce                     = NULL;
+    char *tpmpass                   = NULL;
+    int  sign_tpm                   = 0;
+
+    char *result                    = NULL;
+    size_t rsize                    = 0;
+
+    int ret_val                     = 0;
+
+    // Parse args
+    errno = 0;
+    if((argc != 10) ||
+       (((out_fd = strtol(argv[2], NULL, 10)) < 0) || errno != 0) ||
+       (((targ_fd = strtol(argv[3], NULL, 10)) < 0) || errno != 0) ||
+       (((sign_tpm = strtol(argv[10], NULL, 10)) < 0) || errno != 0)){
+        asp_logerror("Usage: "ASP_NAME" <in_fd [unused]> <out_fd> <targ_fd> <resource> <certfile> <keyfile> <keypass> <nonce> <tpmpass> <sign_tpm>\n");
+        return -EINVAL;
+    }
+
+    resource = argv[4];
+    certfile = argv[5];
+    keyfile  = argv[6];
+    keypass  = argv[7];
+    nonce    = argv[8];
+    tpmpass  = argv[9];
+
+    ret_val = send_to_attester_listen_for_result(targ_fd, resource, certfile, keyfile,
+              keypass, nonce, tpmpass, sign_tpm, &result, &rsize);
+    if(ret_val < 0) {
+      dlog(0, "Unable to send execute contract to attester or get a result\n");
+      goto error;
+    }
+
+    // Write the result back to the invoking APB
+    ret_val = maat_write_sz_buf(out_fd, result, rsize, NULL, 2);
+    if(ret_val != 0) {
+      dlog(0, "Error sending request. returned status is %d: %s\n", ret_val,
+	   strerror(-ret_val));
+      ret_val = -1;
+      goto error;
+    }
+
+    ret_val = 0;
+
+error:
+    free(result);
+    return ret_val;
+}
